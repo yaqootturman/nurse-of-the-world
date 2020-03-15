17aaae5deec01afd5fda60a9600c0de9b287edbb
hadeel salah <hadeelsalah80@gmail.com>
2020-03-15 17:40:16 +0200

    update home layout

--- client/src/Components/ChangeableComponents/Home/Home.js
+++ client/src/Components/ChangeableComponents/Home/Home.js
@@ -8,6 +8,7 @@ import {TweenMax,Power3} from 'gsap';
 function Home () {
     let home = useRef(null);
     let breif = useRef(null);
+    let seconddivlogo = useRef(null);
     useEffect(() => {
         TweenMax.to(
             home,
@@ -24,6 +25,15 @@ function Home () {
             {
                 ease: Power3.easeInOut,
                 delay: 1,
+                opacity: 1
+            }
+        )
+        TweenMax.to(
+            seconddivlogo,
+            .8,
+            {
+                ease: Power3.easeInOut,
+                delay: 3,
                 opacity: 1
             }
         )
@@ -46,7 +56,8 @@ function Home () {
                 </div>
                 <div className="div-second">
                     <div class="para">
-                        <p >مهتمنا هي  تعزيز كفاءة التعليم والتدريب والدور القيادي للشباب عن طريق دعم جيل جديد من الشباب ليصبحوا قادة متعلمين ، مهنيين مهرة ، ورواداَ يلهمون أفراد عائلاتهم للتكاثف وتحقيق الازدهار</p>
+                        <p ref={el => (seconddivlogo = el)} >نحن فريق شبابي طبي نتطلع إلى جيل حر قادر متعلم ذو كفاءة عن طريق مساعدة بعضنا
+                            البعض، نشبك أيدينا سوياُ لنحمل مستقبل أفضل كل شبابنا ومجتمعنا</p>        
                     </div>
                     <div className="icons">
                     <ul>
@@ -61,15 +72,45 @@ function Home () {
                     
                 </div>
                 <div className="div-third">
+                    <div className="tittle"></div>
+                    <div className="activites-shape"></div>
+                    <button>استكشاف المزيد</button>
+                    <div>try</div>
+                    <div>try</div>
+                    <div>try</div>
+                    <div>try</div>
+                    <div>try</div>
+
                     
                 </div>
                 <div className="div-fourth">
-                    
+                    <div className="article-shape">
+                        <div className="first-item">
+                          1   
+                        </div>
+                         <div className="second-item">
+                           2 
+                        </div>
+                        <div className="third-item">
+                             3  
+                        </div>
+                        <h1>tittle1</h1>
+                        <h1 >tittle2</h1>
+                        <h1 >tittle3</h1> 
+                        </div> 
+                        
+                    <button>استكشاف المزيد</button>
+
                     </div>
                 <div className="div-fifth">
                     
                 </div>
                 <div className="div-sixth">
+                    <h1>كن متطوعاَ</h1>
+                    <p>بهدف مساعدة أكبر عدد ممكن من  الأشخاص ، نفتقر دائما إلى المتطوعين المتحمسين
+                       <mark> ، إن كنت تريد صنع تغيير في العالم وتتبنى رسالتنا وأهدافنا</mark>
+                        يرجى تقديم طلب تطوع أو مراسلتنا </p>
+                 <a href="https://www.google.com/"  target="_blank" title="طلب تطوع"><button>تقديم الطلب </button></a>
                     
                     </div>
             </div>
@@ -77,4 +118,5 @@ function Home () {
             </>
         )
     }
+
 export default Home;
