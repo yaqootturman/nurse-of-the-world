061d9d944102f14397625e05b0a490cf86d44b24
yaqootturman <yaqoot.turman@gmail.com>
2020-03-16 15:51:55 +0200

    dataabase

---
+++ server/database/db_build.sql
@@ -0,0 +1,27 @@
+BEGIN;
+
+DROP TABLE IF EXISTS event, article, activity
+    CASCADE;
+
+CREATE TABLE event
+(
+    event_id serial primary key ,
+    title VARCHAR(100) ,
+    content TEXT NOT NULL
+);
+
+CREATE TABLE article
+(
+    article_id serial primary key ,
+    title VARCHAR(700) ,
+    content TEXT NOT NULL
+);
+
+CREATE TABLE activity
+(
+    activity_id serial primary key ,
+    title VARCHAR(100) ,
+    content TEXT NOT NULL
+);
+
+COMMIT;
