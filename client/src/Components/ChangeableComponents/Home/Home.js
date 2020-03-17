247c2987af4eb117d18ef03b8adde9c3c70eb02e
hadeel salah <hadeelsalah80@gmail.com>
2020-03-17 22:27:25 +0200

    Upadate Home layout

--- client/src/Components/ChangeableComponents/Home/Home.js
+++ client/src/Components/ChangeableComponents/Home/Home.js
@@ -1,134 +1,29 @@
-import React, { Component,useRef, useEffect}from 'react'
+import React from 'react'
 import './home.css';
 import Footer from '../../FixedComponents/Footer/Footer';
 import Header from '../../FixedComponents/Header/Header';
-import homeLogo from './homeLogo.png';
-import {TweenMax,Power3} from 'gsap';
+import FirstDiv from '../FirstUISection/FirstDiv';
+import WhoWeAre from '../WhoWeAreSection/WhoWeAre';
+import EventNews from '../EventsandNewsSection/EventNews';
+import Calendar from '../CalendarSection/Calendar';
+import Articles from '../ArticlesSection/Articles';
+import Volunteer from '../VolunteerSection/Volunteer';
+
 
 function Home () {
-    let home = useRef(null);
-    let breif = useRef(null);
-    let seconddivlogo = useRef(null);
-    useEffect(() => {
-        TweenMax.to(
-            home,
-            .8,
-            {
-                ease: Power3.easeInOut,
-                delay: 1.2,
-                opacity: 1
-            }
-        )
-        TweenMax.to(
-            breif,
-            .8,
-            {
-                ease: Power3.easeInOut,
-                delay: 1,
-                opacity: 1
-            }
-        )
-        TweenMax.to(
-            seconddivlogo,
-            .8,
-            {
-                ease: Power3.easeInOut,
-                delay: 3,
-                opacity: 1
-            }
-        )
-    }, [])        
+      
 
         return (
             <>
             <Header></Header>
             <div class="all-home">
-                <div className="div-one">
-                    <div className="div-one-logo">
-                        <img  ref={el => (home = el)} src={homeLogo}></img>
-                    </div>
-                    <div className="paragraph">
-                        <p ref={el => (breif = el)}>مهتمنا هي  تعزيز كفاءة التعليم والتدريب والدور القيادي للشباب عن طريق دعم جيل جديد من الشباب ليصبحوا قادة متعلمين ، مهنيين مهرة ، ورواداَ يلهمون أفراد عائلاتهم للتكاثف وتحقيق الازدهار</p>
-                        <button>المزيد عنا</button>
-                    </div>
-                  
- 
-                </div>
-                <div className="div-second">
-                    <div class="para">
-                        <p ref={el => (seconddivlogo = el)} >نحن فريق شبابي طبي نتطلع إلى جيل حر قادر متعلم ذو كفاءة عن طريق مساعدة بعضنا
-                            البعض، نشبك أيدينا سوياُ لنحمل مستقبل أفضل كل شبابنا ومجتمعنا</p>        
-                    </div>
-                    <div className="icons">
-                    <ul>
-                        <li><a href="https://www.google.com/" target="_blank"  title="الأهداف"><i class="fas fa-bullseye-arrow fa-3x"></i> </a></li>
-                        <li><a href="https://www.google.com/" target="_blank" title="القيم"><i class="fas fa-file fa-3x"></i></a></li>
-                        <li><a href="https://www.google.com/"  target="_blank"  title="رؤية الفريق ومهامه"><i class="fas fa-eye fa-3x"></i></a></li>
-                        <li><a href="https://www.google.com/" target="_blank"  title="من نحن؟"> <i class="fas fa-users fa-3x"></i></a></li>
-
-                    </ul>
-                        
-                    </div>
-                    
-                </div>
-                <div className="div-third">
-                    <div className="tittle">
-                        <h1>قسم الأنشطة والأخبار</h1>
-                        <hr></hr> <hr></hr>
-                    </div>
-                    <div className="activites-shape">
-                    <div className="first">
-                          1   
-                        </div>
-                         <div className="second">
-                           2 
-                        </div>
-                        <div className="third">
-                             3  
-                        </div>
-                        <h1>tittle1</h1>
-                        <h1 >tittle2</h1>
-                        <h1 >tittle3</h1> 
-                     </div>
-                     <button>استكشاف المزيد</button>
-                        </div> 
-
-                
-                <div className="div-fifth">
-                    
-                </div>
-                <div className="div-fourth">
-                    <div className="tittle">
-                        <h1>قسم المقالات </h1>
-                        <hr></hr> <hr></hr>
-                    </div>
-                    <div className="article-shape">
-                        <div className="first-item">
-                          1   
-                        </div>
-                         <div className="second-item">
-                           2 
-                        </div>
-                        <div className="third-item">
-                             3  
-                        </div>
-                        <h1>tittle1</h1>
-                        <h1 >tittle2</h1>
-                        <h1 >tittle3</h1> 
-                        </div> 
-                        
-                    <button>استكشاف المزيد</button>
-
-                    </div>
-                <div className="div-sixth">
-                    <h1>كن متطوعاَ</h1>
-                    <p>بهدف مساعدة أكبر عدد ممكن من  الأشخاص ، نفتقر دائما إلى المتطوعين المتحمسين
-                       <mark> ، إن كنت تريد صنع تغيير في العالم وتتبنى رسالتنا وأهدافنا</mark>
-                        يرجى تقديم طلب تطوع أو مراسلتنا </p>
-                 <a href="https://www.google.com/"  target="_blank" title="طلب تطوع"><button>تقديم الطلب </button></a>
-                    
-                    </div>
-            </div>
+               <FirstDiv></FirstDiv>
+               <WhoWeAre></WhoWeAre>
+               <EventNews></EventNews>
+               <Calendar></Calendar>
+               <Articles></Articles>
+               <Volunteer></Volunteer>
+             </div>
             <Footer></Footer>
             </>
         )
