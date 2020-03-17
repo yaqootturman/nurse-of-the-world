9f265e6810b70bac548d55ce942808b03ba1b837
hadeel salah <hadeelsalah80@gmail.com>
2020-03-17 22:29:39 +0200

    Create and build volunteer section

---
+++ client/src/Components/ChangeableComponents/VolunteerSection/Volunteer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./volunteer.css";
+
+
+function Volunteer(){
+        return(
+            <div className="div-sixth">
+                <h1>كن متطوعاَ</h1>
+                <p>بهدف مساعدة أكبر عدد ممكن من  الأشخاص ، نفتقر دائما إلى المتطوعين المتحمسين
+                <mark> ، إن كنت تريد صنع تغيير في العالم وتتبنى رسالتنا وأهدافنا</mark>
+                    يرجى تقديم طلب تطوع أو مراسلتنا </p>
+                <a href="https://www.google.com/"  target="_blank" title="طلب تطوع"><button>تقديم الطلب </button></a>
+
+            </div>
+
+        )
+    }
+export default Volunteer;
