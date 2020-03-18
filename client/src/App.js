3d5ad17c509437c15a5a571e4f885f0b4ecb6de2
hadeel salah <hadeelsalah80@gmail.com>
2020-03-18 22:26:29 +0200

    Add route link for main article page and event page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from './Components/ChangeableComponents/Home/Home';
+import AllArticles from './Components/ChangeableComponents/ArticlesSection/AllArticles';
+import NewsMainPage from './Components/ChangeableComponents/EventsandNewsSection/NewsMainPage';
+
 
 function App() {
   return (
     <Router>
         <Switch>
-          <Home></Home>
+          <Route exact path ="/" component={Home} />
+          <Route exact path ="/home/article-section" component={AllArticles} />
+          <Route exact path ="/home/news-and-event-section" component={NewsMainPage} />
+
+
         </Switch> 
        </Router>
   );
