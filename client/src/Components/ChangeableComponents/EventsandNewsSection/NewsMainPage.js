7abfbe67a423363a29bb64b38953c507e4ce6496
hadeel salah <hadeelsalah80@gmail.com>
2020-03-18 22:27:25 +0200

    Create Main Page for news

---
+++ client/src/Components/ChangeableComponents/EventsandNewsSection/NewsMainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function NewsMainPage(){
+  
+        return(
+            <div>
+                <h1>kdkdkdkd</h1>
+                    
+            </div>
+       
+
+            )
+    }
+
+export default NewsMainPage;
