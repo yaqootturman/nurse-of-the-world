d7b4742e5a913bcb683950eb26e87a31de091b9e
hadeel salah <hadeelsalah80@gmail.com>
2020-03-19 14:52:20 +0200

    Update app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from './Components/ChangeableComponents/Home/Home';
-import AllArticles from './Components/ChangeableComponents/ArticlesSection/AllArticles';
-import NewsMainPage from './Components/ChangeableComponents/EventsandNewsSection/NewsMainPage';
+import AllArticles from './Components/ChangeableComponents/Articles/MainPageComponent/AllArticles';
+import NewsMainPage from './Components/ChangeableComponents/EventsandNewsSection/EventsNewsPageComponent/NewsMainPage';
+import FirstDivMainPage from './Components/ChangeableComponents/FirstUISection/MainPageForFirstSection/FirstDivMainPage';
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
           <Route exact path ="/" component={Home} />
           <Route exact path ="/home/article-section" component={AllArticles} />
           <Route exact path ="/home/news-and-event-section" component={NewsMainPage} />
+          <Route exact path ="/home/details-about-us" component={FirstDivMainPage} />
+
 
 
         </Switch> 
