a7ec2b8556480fca2a82df85a20bbd366ee1d4d5
hadeel salah <hadeelsalah80@gmail.com>
2020-03-19 15:03:39 +0200

    create news main page js and css files

---
+++ client/src/Components/ChangeableComponents/EventsandNewsSection/EventsNewsPageComponent/NewsMainPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Footer from '../../../FixedComponents/Footer/Footer';
+import Header from '../../../FixedComponents/Header/Header';
+import './newsmainpage.css';
+    
+    
+
+function NewsMainPage(){      
+        return(
+            <>
+               <Header></Header>
+                <div className="news-all">
+                <h1>jdjdjd</h1>
+                        
+                </div>
+                <Footer></Footer>
+    
+           </>
+        )
+    }
+
+export default NewsMainPage;
