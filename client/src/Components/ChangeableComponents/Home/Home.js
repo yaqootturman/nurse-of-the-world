156bf1ab25ce4a4b5686b834093dec41cec11af2
hadeel salah <hadeelsalah80@gmail.com>
2020-03-19 14:52:56 +0200

    Update home file

--- client/src/Components/ChangeableComponents/Home/Home.js
+++ client/src/Components/ChangeableComponents/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './home.css';
+import {Link} from 'react-router-dom';
 import Footer from '../../FixedComponents/Footer/Footer';
 import Header from '../../FixedComponents/Header/Header';
 import FirstDiv from '../FirstUISection/FirstDiv';
 import WhoWeAre from '../WhoWeAreSection/WhoWeAre';
-import EventNews from '../EventsandNewsSection/EventNews';
+import EventNews from '../EventsandNewsSection/EventsNewsSection/EventNews';
 import Calendar from '../CalendarSection/Calendar';
-import Articles from '../ArticlesSection/Articles';
+import Articles from '../Articles/ArticleSection/Articles';
 import Volunteer from '../VolunteerSection/Volunteer';
 
 
@@ -15,15 +16,15 @@ function Home () {
 
         return (
             <>
-            <Header></Header>
-            <div class="all-home">
-               <FirstDiv></FirstDiv>
-               <WhoWeAre></WhoWeAre>
-               <EventNews></EventNews>
-               <Calendar></Calendar>
-               <Articles></Articles>
-               <Volunteer></Volunteer>
-             </div>
+             <Header></Header>
+                <div class="all-home">
+                    <FirstDiv></FirstDiv>
+                    <WhoWeAre></WhoWeAre>
+                    <EventNews></EventNews>
+                    <Calendar></Calendar>
+                    <Articles></Articles>
+                    <Volunteer></Volunteer>
+                </div>
             <Footer></Footer>
             </>
         )
