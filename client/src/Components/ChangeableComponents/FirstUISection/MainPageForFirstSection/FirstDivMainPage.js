2ad3201eb02f74c0e2219de2058efba7003c9350
hadeel salah <hadeelsalah80@gmail.com>
2020-03-19 14:55:14 +0200

    create main page for the first section

---
+++ client/src/Components/ChangeableComponents/FirstUISection/MainPageForFirstSection/FirstDivMainPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Footer from '../../../FixedComponents/Footer/Footer';
+import Header from '../../../FixedComponents/Header/Header';
+import './firstdivmainpage.css';
+
+
+function FirstDivMainPage(){
+  
+        return(
+            <>
+            <Header></Header>
+            <div className="main-all">
+            <h1>jdjdjd</h1>
+                    
+            </div>
+            <Footer></Footer>
+
+            </>
+
+            )
+    }
+
+export default FirstDivMainPage;
