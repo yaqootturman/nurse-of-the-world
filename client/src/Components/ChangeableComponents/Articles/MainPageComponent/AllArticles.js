b6ca71f29dd7c127fb9356907146fee56ba62bda
hadeel salah <hadeelsalah80@gmail.com>
2020-03-19 14:56:42 +0200

    create the main page for articles page

---
+++ client/src/Components/ChangeableComponents/Articles/MainPageComponent/AllArticles.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Footer from '../../../FixedComponents/Footer/Footer';
+import Header from '../../../FixedComponents/Header/Header';
+import './allarticles.css';
+
+function AllArticles() {
+    return (
+        <>
+          <Header></Header>
+          <div className="all-articles">
+          <h1>jdjdjd</h1>
+                  
+          </div>
+          <Footer></Footer>
+
+</>
+  
+  );
+}
+
+export default AllArticles;
