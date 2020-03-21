7d85f03c80e2b4c9baa8072aa10e4ed709816459
hadeel salah <hadeelsalah80@gmail.com>
2020-03-21 22:03:29 +0200

    Create secondary header

---
+++ client/src/Components/ChangeableComponents/SecondaryHeader/SecondaryHeader.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import './secondaryHeader.css';
+
+export default function SecondartHeader() {
+    return (
+        <div className="secondary-header">
+            <div className="left"> 
+                <div className="icon">
+                    <Link to='/'>
+                        <i className="fas fa-arrow-left fa-3x"> 
+                        </i>
+                    </Link>
+                </div>
+                <h1>العودة الى الصفحة الرئيسية </h1>
+            </div>
+            <div className="right">
+            <h1><span>فريق</span> ممرض العالم / قسم من نحن </h1>
+
+            </div>
+
+
+        </div>
+    )
+}
