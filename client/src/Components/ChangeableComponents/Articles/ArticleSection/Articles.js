1047de4583f6c5e872c2302e34e61c95de26f973
hadeel salah <hadeelsalah80@gmail.com>
2020-03-22 15:42:13 +0200

    add arrows for back and forward movement

--- client/src/Components/ChangeableComponents/Articles/ArticleSection/Articles.js
+++ client/src/Components/ChangeableComponents/Articles/ArticleSection/Articles.js
@@ -7,26 +7,28 @@ function Articles(){
   
         return(
             <div id="section5" className="div-fourth">
-            <div className="tittle">
-                <h1>قسم المقالات </h1>
-                <hr></hr> <hr></hr>
-            </div>
-            <div className="article-shape">
-                <div className="first-item">
-                  1   
-                </div>
-                 <div className="second-item">
-                   2 
-                </div>
-                <div className="third-item">
-                     3  
-                </div>
-                <h1>tittle1</h1>
-                <h1 >tittle2</h1>
-                <h1 >tittle3</h1> 
-                </div> 
-                
-                <Link to="/home/article-section"> <button>استكشاف المزيد</button></Link>
+             
+             <i id="arrow-left-article" class="fas fa-arrow-left"></i>                
+              <i id="arrow-right" class="fas fa-arrow-right"></i>
+              <div className="tittle">
+                  <h1>قسم المقالات </h1>
+                  <hr></hr> <hr></hr>
+              </div>
+              <div className="article-shape">
+                  <div className="first-item">
+                    1   
+                  </div>
+                  <div className="second-item">
+                    2 
+                  </div>
+                  <div className="third-item">
+                      3  
+                  </div>
+                  <h1>tittle1</h1>
+                  <h1 >tittle2</h1>
+                  <h1 >tittle3</h1> 
+                  </div> 
+                  <Link to="/home/article-section"> <button>استكشاف المزيد</button></Link>
 
             </div>
        
