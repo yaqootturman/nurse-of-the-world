20b959108566fd2150d256055663cf6970cf2af4
hadeel salah <hadeelsalah80@gmail.com>
2020-03-24 16:19:24 +0200

    add Calendar section

--- client/src/Components/ChangeableComponents/Home/Home.js
+++ client/src/Components/ChangeableComponents/Home/Home.js
@@ -1,25 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './home.css';
 import {Link} from 'react-router-dom';
 import Footer from '../../FixedComponents/Footer/Footer';
 import Header from '../../FixedComponents/Header/Header';
 import FirstDiv from '../FirstUISection/FirstDiv';
 import EventNews from '../EventsandNewsSection/EventsNewsSection/EventNews';
-import Calendar from '../CalendarSection/Calendar';
 import Articles from '../Articles/ArticleSection/Articles';
 import Volunteer from '../VolunteerSection/Volunteer';
+import CalendarSection from '../CalendarSection/CalendarSection';
 
-
-function Home () {
-      
-
+class Home extends Component {
+    render(){
         return (
             <>
              <Header></Header>
                 <div id="section2" class="all-home">
                     <FirstDiv ></FirstDiv>
                     <EventNews></EventNews>
-                    <Calendar></Calendar>
+                    <CalendarSection></CalendarSection>
                     <Articles></Articles>
                     <Volunteer></Volunteer>
                 </div>
@@ -27,5 +25,6 @@ function Home () {
             </>
         )
     }
+}
 
 export default Home;
