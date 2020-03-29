06df6eeb867287d4de728e8162c458418b76a3b1
hadeel salah <hadeelsalah80@gmail.com>
2020-03-29 14:43:47 +0300

    Add link to the icon to connect pages

--- client/src/Components/ChangeableComponents/SecondaryHeader/SecondaryHeader.js
+++ client/src/Components/ChangeableComponents/SecondaryHeader/SecondaryHeader.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react'
 import './secondaryHeader.css';
-import history from '../../../history';
-
+import { Link } from 'react-router-dom';
 export default class SecondartHeader extends Component {
-    handleClick(){
-        history.push('/')
-
-    }
     render(){
     return (
         <div className="secondary-header">
             <div className="left"> 
                 <div className="icon">
-                   <i className="fas fa-arrow-left fa-3x">
-                   </i>
-                   <button onClick={(e) =>this.handleClick(e)}>hi </button>
-
+                    <Link to='/home'>
+                        <i className="fas fa-arrow-left fa-3x">
+                        </i>
+                   </Link>
                 </div>
                 <h1>العودة الى القسم الرئيسي</h1>
             </div>
