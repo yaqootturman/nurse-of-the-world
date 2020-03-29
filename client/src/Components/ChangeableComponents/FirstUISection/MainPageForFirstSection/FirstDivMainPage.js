7f365606102c0c6abf68dbf7aa8c9b90e5d7d3c4
hadeel salah <hadeelsalah80@gmail.com>
2020-03-29 14:44:13 +0300

    Remove all style fuctions

--- client/src/Components/ChangeableComponents/FirstUISection/MainPageForFirstSection/FirstDivMainPage.js
+++ client/src/Components/ChangeableComponents/FirstUISection/MainPageForFirstSection/FirstDivMainPage.js
@@ -1,5 +1,4 @@
-import React ,{useRef, useEffect} from 'react';
-import {TweenMax,Power3} from 'gsap';
+import React from 'react';
 import SecondaryHeader from '../../SecondaryHeader/SecondaryHeader'
 import Footer from '../../../FixedComponents/Footer/Footer';
 import "./firstdivmainpage.css";
@@ -8,84 +7,67 @@ import "./firstdivmainpage.css";
 function FirstDivMainPage(){
     window.scroll({
         top: 0, 
-      });
-        let words= useRef(null);
-        let listFour = useRef(null);
-        let listOne = useRef(null);
-        let listTwo = useRef(null);
-        let listThree = useRef(null);
-
-
-        useEffect(()=>{
-            TweenMax.to(
-                words,
-                .8,
-                {
-                    ease: Power3.easeInOut,
-                    delay: 2.4,
-                    opacity: 1
-                }
-            )
-            TweenMax.to(
-                listOne,
-                .8,
-                {
-                    ease: Power3.easeInOut,
-                    delay: 3.4,
-                    opacity: 1
-                }
-            )
-            TweenMax.to(
-                listTwo,
-                .8,
-                {
-                    ease: Power3.easeOut,
-                    delay: 3.2,
-                    opacity: 1
-                }
-            )
-            TweenMax.to(
-                listThree,
-                .8,
-                {
-                    ease: Power3.easeInOut,
-                    delay: 3,
-                    opacity: 1
-                }
-            )
-            TweenMax.to(
-                listFour,
-                .8,
-                {
-                    ease: Power3.easeInOut,
-                    delay: 2.8,
-                    opacity: 1
-                }
-            )
-        }, [])   
+      });  
     return(
         <>
         <SecondaryHeader></SecondaryHeader>
-        <div className="div-second">
-        <div class="para">
-            <p ref={el => (words = el)} >نحن فريق شبابي طبي نتطلع إلى جيل حر قادر متعلم ذو كفاءة عن طريق مساعدة بعضنا
-                البعض، نشبك أيدينا سوياُ لنحمل مستقبل أفضل كل شبابنا ومجتمعنا</p>        
-        </div>
-        <div className="icons">
-        <ul> 
-            <li ref={el => (listOne = el)}><a href="https://www.google.com/" target="_blank"  title="الأهداف"><i class="fas fa-bullseye-arrow fa-3x"></i> </a></li>
-            <li ref={el => (listTwo = el)}><a href="https://www.google.com/" target="_blank" title="القيم"><i class="fas fa-file fa-3x"></i></a></li>
-            <li ref={el => (listThree = el)}><a href="https://www.google.com/"  target="_blank"  title="رؤية الفريق ومهامه"><i class="fas fa-eye fa-3x"></i></a></li>
-            <li ref={el => (listFour = el)}><a href="https://www.google.com/" target="_blank"  title="من نحن؟"> <i class="fas fa-users fa-3x"></i></a></li>
+        <div className="all-p">
+            <div className="div-second">
+                <div class="main-section">
+                     <p>نحن فريق شبابي طبي نتطلع إلى جيل حر قادر متعلم ذو كفاءة عن طريق مساعدة بعضنا
+                    البعض، نشبك أيدينا سوياُ لنحمل مستقبل أفضل كل شبابنا ومجتمعنا</p>        
+                </div>
+                <div className="icons">
+                    <div className="first-icon">
+                            <a className="hover1" title="من نحن؟">
+                            <i class="fas fa-users fa-3x"></i>
+                                <h1>فريق شبابي طبي تأسس في أكتوبر 2017 نتطلع إلى تطوير المعرفة العلمية والكفاءة العملية
+                    والتنمية لدى طلبة الجامعات ونشر الوعي الصحي في المجتمع</h1>
+                            </a>
+                            
+                    </div>
+                    <div className="second-icon">
+                           <a className="hover2" title="رؤية الفريق" >  
+                           <i class="fas fa-eye fa-3x"></i>
+                                <h1>
+                                    مجتمع معرفي يتبنى الحوار ويقدر العلم والصحة وله دور فعّال في تمكين الشباب وتحفيزهم
+                                </h1>
+                            </a>
+                            
+                    </div>
+                    <div className="third-icon">
+                        <a className="hover3" title="القيم">
+                        <i className="fas fa-bullseye-arrow fa-3x"></i>
+                        <h1>
+                                    <li> الحوار وإنتاج المعرفة</li> 
+                                    <li>تقدير قيمة العمل بروح الزمالة وتثمين الانتاج</li> 
+                                    <li> الابداع والابتكار</li>
+                                    <li>  الاعتمادية والكفاءة</li>   
+                                    <li>القيادة المشتركة </li>
 
-        </ul>
-        <div className="first-icon">
-            <p>jdjdjjdjddjd</p>
-        </div>
+                        </h1>
+                        </a>
+                          
+                        </div>
+                    <div className="fourth-icon">
+                            <a className="hover4" title="أهداف الفريق">
+                            <i class="fas fa-file fa-3x"></i>
+                            <h1>
+                                    <li>تحفيز مشاركة الشباب في المجتمع</li>
+                                    <li>زيادة وتحسين المعرفة العلمية والمهنية لدى طلبة الجامعات</li>
+                                    <li>نشر الوعي الصحي واالجتماعي في المجتمع</li>
+                                    <li>تمكين أدوار الشباب عن طريق دمجهم في برامج وفعاليات ضمن المجتمع</li>
+                                    <li>توسيع الفريق وزيادة قدراته وإمكانياته</li>
+                            </h1>
+                            </a>
+                           
+                    </div>
+                      
+                
+            </div>
             
         </div>
-        
-    </div>
+     </div>
     <Footer></Footer>
     </>
 
