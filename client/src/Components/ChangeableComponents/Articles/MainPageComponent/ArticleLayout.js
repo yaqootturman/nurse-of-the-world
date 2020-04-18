ee9d3034cf70d40774c005cad45d60733373fc78
hadeel salah <hadeelsalah80@gmail.com>
2020-04-18 17:14:43 +0300

    Build the article component for the article content

---
+++ client/src/Components/ChangeableComponents/Articles/MainPageComponent/ArticleLayout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './articleLayout.css';
+function ArticleLayout(props){
+    return(
+        <div className='popup2'>  
+        <div className='inner'> 
+              <div className="content">
+                 <h1>{props.header}</h1>
+                  <p> {props.text} </p>
+                 
+             </div>
+             <button>
+                 close
+             </button>  
+        </div>  
+    </div>  
+    )
+}
+export default ArticleLayout;
