58899f8ed89a263d58e7ae024b8ee4c35a3f2580
hadeel salah <hadeelsalah80@gmail.com>
2020-04-22 23:41:36 +0300

    Add alt for img

--- client/src/Components/ChangeableComponents/FirstUISection/FirstDiv.js
+++ client/src/Components/ChangeableComponents/FirstUISection/FirstDiv.js
@@ -33,7 +33,7 @@ function FirstDiv(){
 
             <div className="div-one">
                 <div className="div-one-logo">
-                    <img  ref={el => (home = el) } src={homeLogo}></img>
+                    <img alt="logo" ref={el => (home = el) } src={homeLogo}></img>
                 </div>
                 <div className="paragraph">
                     <p ref={el => (breif = el)}>مهتمنا هي  تعزيز كفاءة التعليم والتدريب والدور القيادي للشباب عن طريق دعم جيل جديد من الشباب ليصبحوا قادة متعلمين ، مهنيين مهرة ، ورواداَ يلهمون أفراد عائلاتهم للتكاثف وتحقيق الازدهار</p>
