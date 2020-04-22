9fe4a577264125d756d33a88dc2b3ad2674997ab
hadeel salah <hadeelsalah80@gmail.com>
2020-04-22 23:38:43 +0300

    Add key for every item

--- client/src/Components/ChangeableComponents/EventsandNewsSection/EventsNewsSection/EventNews.js
+++ client/src/Components/ChangeableComponents/EventsandNewsSection/EventsNewsSection/EventNews.js
@@ -2,16 +2,9 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import "./eventNews.css";    
 class EventNews extends Component{
-    //eventsData
-    render(){
-        var settings = {
-            dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
-          };
 
+    render(){
+        
         return(
             <div id="section3" className="div-third">
                 <div className="tittle">
@@ -20,7 +13,7 @@ class EventNews extends Component{
                 </div>
                 <div className="activites-shape">
                     {this.props.eventsData.map( event =>
-                         <div className="first">
+                         <div key={event.activity_id} className="first">
                              <h1>{event.title}</h1>
                       
                          </div>
