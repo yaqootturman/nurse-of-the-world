bbcdacb166877bfb80c9abd7cff26171ade4f07f
yaqootturman <yaqoot.turman@gmail.com>
2020-08-06 11:03:13 +0300

    change the style of apply button

--- client/src/Components/ChangeableComponents/VolunteerSection/Volunteer.js
+++ client/src/Components/ChangeableComponents/VolunteerSection/Volunteer.js
@@ -2,17 +2,17 @@ import React from 'react';
 import "./volunteer.css";
 
 
-function Volunteer(){
-        return(
-            <div id="section7" className="div-sixth">
-                <h1>كن متطوعاَ</h1>
-                <p>بهدف مساعدة أكبر عدد ممكن من  الأشخاص ، نفتقر دائما إلى المتطوعين المتحمسين
-                 ، إن كنت تريد صنع تغيير في العالم وتتبنى رسالتنا وأهدافنا
+function Volunteer() {
+    return (
+        <div id="section7" className="div-sixth">
+            <h1>كن متطوعاَ</h1>
+            <p>بهدف مساعدة أكبر عدد ممكن من  الأشخاص ، نفتقر دائما إلى المتطوعين المتحمسين
+            ، إن كنت تريد صنع تغيير في العالم وتتبنى رسالتنا وأهدافنا
                     يرجى تقديم طلب تطوع أو مراسلتنا </p>
-                <a href="https://www.google.com/" title="طلب تطوع"><button>تقديم الطلب </button></a>
+            <a href="https://www.google.com/" title="طلب تطوع"><button>تقديم طلب</button></a>
 
-            </div>
+        </div>
 
-        )
-    }
+    )
+}
 export default Volunteer;
